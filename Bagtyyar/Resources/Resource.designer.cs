77a4f4afd852360c2c4f9fcec6307b8f8c36af44
Maksat <amangeldiyev.m@gmail.com>
2016-07-28 17:17:04 +0500

    Added styles for buttons on Languages page

--- Bagtyyar/Resources/Resource.designer.cs
+++ Bagtyyar/Resources/Resource.designer.cs
@@ -45,7 +45,43 @@ namespace Bagtyyar
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int SplashBackground = 2130837504;
+			public const int EN = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int EN_press = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int EN_sel = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int EnButton = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int RU = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int RU_press = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int RU_sel = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int RuButton = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int SplashBackground = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int TM = 2130837513;
+			
+			// aapt resource value: 0x7f02000a
+			public const int TM_press = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int TM_sel = 2130837515;
+			
+			// aapt resource value: 0x7f02000c
+			public const int TmButton = 2130837516;
 			
 			static Drawable()
 			{
--- Bagtyyar/SplashActivity.cs
+++ Bagtyyar/SplashActivity.cs
@@ -43,8 +43,6 @@ namespace Bagtyyar
 
 			CallLanguagesPage();
 		}
-
-	
 	}
 }
 
